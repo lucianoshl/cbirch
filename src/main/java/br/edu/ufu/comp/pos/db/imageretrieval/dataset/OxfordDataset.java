82259cb8e427f80e3f98c74a957ffee6d7981428
Luciano Lopes <lucianos@algartelecom.com.br>
2016-03-03 12:07:41 -0300

    fixes

--- src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/dataset/OxfordDataset.java
+++ src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/dataset/OxfordDataset.java
@@ -108,9 +108,9 @@ public class OxfordDataset extends Dataset {
 	    String fileName = scanner.nextLine().replace("oxc1_", "") + ".jpg";
 	    c.accept(fileName);
 	    i++;
-	     if (i == 100) {
-	     break;
-	     }
+//	     if (i == 100) {
+//	     break;
+//	     }
 	}
 	scanner.close();
     }
