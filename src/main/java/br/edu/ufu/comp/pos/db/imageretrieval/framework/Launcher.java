7ebf95891c3512022c1cb656587628919a9b1c17
Luciano Lopes <lucianos@algartelecom.com.br>
2016-03-08 20:47:05 -0300

    fix logger error

--- src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/framework/Launcher.java
+++ src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/framework/Launcher.java
@@ -19,21 +19,24 @@ public class Launcher {
 	if (args.length == 0) {
 	    throw new IllegalArgumentException("tree name is required");
 	}
-	
+
 	Dataset dataset = new DatasetFactory().create(args);
 	ClusterTree tree = new TreeFactory().create(args);
 
 	StopWatch stopWatch = new StopWatch();
 	stopWatch.start();
 
-	Result result = new Framework().run(dataset, tree, 4);
-	
-	logger.info("elapsed time " + stopWatch.getTime());
-	
-	logger.info(result.toString());
-	
-	result.save();
-    }
+	try {
 
+	    new Framework().run(dataset, tree, 4);
+	} catch (Exception e) {
+	    logger.error(e);
+	} finally {
+	    logger.info("elapsed time " + stopWatch.getTime());
+	    logger.info(Result.instance.toString());
+	    Result.instance.save();
+	}
+
+    }
 
 }
