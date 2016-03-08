25d1c37953f3ed0fe48c144bfa9d4734626e79b8
Luciano Lopes <lucianos@algartelecom.com.br>
2016-03-08 12:02:16 -0300

    runner

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 nohup mvn clean install exec:exec -Dexec.args="-classpath %classpath  -XX:MaxDirectMemorySize=512m -Xmx6656m -javaagent:$(pwd)/SizeOf.jar br.edu.ufu.comp.pos.db.imageretrieval.framework.Launcher birch oxford 100 2000 3072" &
+tail -f nohup.out
