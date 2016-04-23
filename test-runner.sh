78a6825c32c799454975216c9ebf54234cda1700
Luciano Lopes <lucianoshl@gmail.com>
2016-04-23 17:18:44 -0300

    fix stack overflow

--- test-runner.sh
+++ test-runner.sh
@@ -1,4 +1,4 @@
 for threshold in 3 25 50 75 100 150 200
 do
-	mvn clean install -DskipTests exec:exec -Dexec.args="-classpath %classpath -Xmx6656m -Xss6656m -XX:+UseCompressedOops -XX:-UseGCOverheadLimit -javaagent:/home/void/workspace/birch-experiment/SizeOf.jar br.edu.ufu.comp.pos.db.imageretrieval.framework.Launcher birch normalized oxford $threshold 0 3072"
+	mvn clean install -DskipTests exec:exec -Dexec.args="-classpath %classpath -Xmx6656m -Xss1G -XX:+UseCompressedOops -XX:-UseGCOverheadLimit -javaagent:/home/void/workspace/birch-experiment/SizeOf.jar br.edu.ufu.comp.pos.db.imageretrieval.framework.Launcher birch normalized oxford $threshold 0 3072"
 done
