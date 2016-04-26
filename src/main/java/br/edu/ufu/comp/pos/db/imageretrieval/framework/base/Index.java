ed85c47c76341acc080b93cb25c9b6ccbe46aadd
Luciano Lopes <lucianos@algartelecom.com.br>
2016-04-26 09:58:24 -0300

    better logs

--- src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/framework/base/Index.java
+++ src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/framework/base/Index.java
@@ -20,49 +20,50 @@ public class Index {
 
     private ClusterTree tree;
     private Histograms histograms;
-    
-    private Map<AbstractTreeNode,Set<Image>> index = new HashMap<AbstractTreeNode,Set<Image>>();
+
+    private Map<AbstractTreeNode, Set<Image>> index = new HashMap<AbstractTreeNode, Set<Image>>();
 
     public Index(ClusterTree tree) {
         this.tree = tree;
         this.histograms = new Histograms(tree.getEntriesAmount());
     }
 
-    public void put(Image img) {  
-    	
-    	double[] histogramContent = new double[tree.getEntriesAmount()];
-    	
-    	img.scan((c)->{
-    		AbstractTreeNode treeNode = tree.findClosestCluster(c);
-    		histogramContent[treeNode.getId()]++;
-    		putInIndex(treeNode,img);
-    	});
-    	
+    public void put(Image img) {
+
+        double[] histogramContent = new double[tree.getEntriesAmount()];
+
+        img.scan((c) -> {
+            AbstractTreeNode treeNode = tree.findClosestCluster(c);
+            histogramContent[treeNode.getId()]++;
+            putInIndex(treeNode, img);
+        });
+
         histograms.add(new Histogram(img, histogramContent));
     }
 
     private void putInIndex(AbstractTreeNode treeNode, Image img) {
-    	Set<Image> list = index.get(treeNode);
-    	if (list == null){
-    		list = new HashSet<Image>();
-    		index.put(treeNode, list);
-    	}
-    	list.add(img);
-		
-	}
+        Set<Image> list = index.get(treeNode);
+        if (list == null) {
+            list = new HashSet<Image>();
+            index.put(treeNode, list);
+        }
+        list.add(img);
 
-	public List<Histogram> findTop(Image query, int k) {
+    }
+
+    public List<Histogram> findTop(Image query, int k) {
         logger.debug("Find top" + k + " for " + query.getImage().getName());
-        
+
         Set<Image> candidates = new HashSet<Image>();
-        
-        query.scan((c)->{
-    		AbstractTreeNode treeNode = tree.findClosestCluster(c);
-    		candidates.addAll(index.get(treeNode));
-    	});
-        
-        
-        return this.histograms.getSimilar(Histogram.create(query, tree),candidates, k);
+
+        query.scan((c) -> {
+            AbstractTreeNode treeNode = tree.findClosestCluster(c);
+            candidates.addAll(index.get(treeNode));
+        });
+
+        logger.debug("candidates number : " + candidates.size());
+
+        return this.histograms.getSimilar(Histogram.create(query, tree), candidates, k);
     }
 
 }
