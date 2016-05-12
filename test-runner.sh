a19f3c706ebc34a1fa609071c5759cf421018fef
Luciano Lopes <lucianos@algartelecom.com.br>
2016-05-12 17:21:27 -0300

    itens

--- test-runner.sh
+++ test-runner.sh
@@ -1,4 +1,4 @@
 for threshold in 2 3 25 50 75 100 150 200
 do
-	mvn clean install -DskipTests exec:exec -Dexec.args="-classpath %classpath -Xmx6656m -Xss1G -XX:+UseCompressedOops -XX:-UseGCOverheadLimit -javaagent:/home/void/workspace/dsl-cbir/SizeOf.jar br.edu.ufu.comp.pos.db.imageretrieval.framework.Launcher birch normalized oxford $threshold 0 20480"
+	mvn clean install -DskipTests exec:exec -Dexec.args="-classpath %classpath -Xmx6656m -Xss1G -XX:+UseCompressedOops -XX:-UseGCOverheadLimit -javaagent:/home/void/pesquisa/source/SizeOf.jar br.edu.ufu.comp.pos.db.imageretrieval.framework.Launcher birch normalized oxford $threshold 0 20480"
 done
