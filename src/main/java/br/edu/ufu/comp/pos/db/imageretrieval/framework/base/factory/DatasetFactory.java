be1363efbcd484dce0ffdbaaaf10a98cf5a0220e
Luciano Lopes <lucianos@algartelecom.com.br>
2016-06-24 12:46:59 -0300

    fix

--- src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/framework/base/factory/DatasetFactory.java
+++ src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/framework/base/factory/DatasetFactory.java
@@ -1,5 +1,10 @@
 package br.edu.ufu.comp.pos.db.imageretrieval.framework.base.factory;
 
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+
 import br.edu.ufu.comp.pos.db.imageretrieval.commons.Utils;
 import br.edu.ufu.comp.pos.db.imageretrieval.dataset.Dataset;
 import br.edu.ufu.comp.pos.db.imageretrieval.dataset.GeneratedDataset;
@@ -7,66 +12,62 @@ import br.edu.ufu.comp.pos.db.imageretrieval.dataset.OxfordDataset;
 import br.edu.ufu.comp.pos.db.imageretrieval.framework.Result;
 import br.edu.ufu.comp.pos.db.imageretrieval.framework.base.sift.Sift;
 import br.edu.ufu.comp.pos.db.imageretrieval.framework.base.sift.SiftScaled;
-import org.apache.log4j.Logger;
 
-import java.io.File;
 
 public class DatasetFactory {
 
-    final static Logger logger = Logger.getLogger(DatasetFactory.class);
+    final static Logger logger = Logger.getLogger( DatasetFactory.class );
 
-    public Dataset create(String[] args) {
 
-        String workspace = System.getenv().get("DATASET_WORKSPACE");
+    public Dataset create( String[] args ) {
 
-        if (args.length < 2) {
-            throw new IllegalArgumentException("A normalização do dataset");
+        String workspace = System.getenv().get( "DATASET_WORKSPACE" );
+
+        if ( args.length < 2 ) {
+            throw new IllegalArgumentException( "A normalização do dataset" );
         }
 
-        if (args.length < 3) {
-            throw new IllegalArgumentException("Informe o dataset");
+        if ( args.length < 3 ) {
+            throw new IllegalArgumentException( "Informe o dataset" );
         }
-        String datasetName = args[2];
+        String datasetName = args[ 2 ];
 
         Sift siftReader = null;
-        if (args[1].equals("normalized")) {
+        if ( args[ 1 ].equals( "normalized" ) ) {
             siftReader = new SiftScaled();
-            Result.extraInfo("Dataset normalized", true);
-        } else if (args[1].equals("non-normalized")) {
+            Result.extraInfo( "Dataset normalized", true );
+        } else if ( args[ 1 ].equals( "non-normalized" ) ) {
             siftReader = new Sift();
-            Result.extraInfo("Dataset normalized", false);
+            Result.extraInfo( "Dataset normalized", false );
         } else {
-            new IllegalStateException("invalid normalized parameter: normalized or non-normalized");
+            new IllegalStateException( "invalid normalized parameter: normalized or non-normalized" );
         }
 
+        File datasetPath = Utils.getDatesetPath( workspace, datasetName );
 
+        Result.extraInfo( "Dataset workspace", workspace );
+        Result.extraInfo( "Dataset name", datasetName );
+        Result.extraInfo( "Dataset path", datasetPath );
 
-        File datasetPath = Utils.getDatesetPath(workspace, datasetName);
-
-        Result.extraInfo("Dataset workspace", workspace);
-        Result.extraInfo("Dataset name", datasetName);
-        Result.extraInfo("Dataset path", datasetPath);
-
-        Result.instance.setDatasetPath(datasetPath);
+        Result.instance.setDatasetPath( datasetPath );
         Dataset dataset = null;
 
-        if (new File(datasetPath, "README2.txt").exists()) {
-            Result.extraInfo("Dataset class", OxfordDataset.class);
-            dataset = OxfordDataset.createFromBase(workspace, datasetName);
-            ((OxfordDataset) dataset).setScanLimit(15);
-        } else if (new File(datasetPath, "train.sift").exists()) {
-            dataset = new GeneratedDataset(datasetName);
+        if ( new File( datasetPath, "README2.txt" ).exists() ) {
+            Result.extraInfo( "Dataset class", OxfordDataset.class );
+            dataset = OxfordDataset.createFromBase( workspace, datasetName );
+            // ((OxfordDataset) dataset).setScanLimit(15);
+        } else if ( new File( datasetPath, "train.sift" ).exists() ) {
+            dataset = new GeneratedDataset( datasetName );
 
         } else {
-            throw new UnsupportedOperationException("unsupported dataset in " + datasetPath.getAbsolutePath());
+            throw new UnsupportedOperationException( "unsupported dataset in " + datasetPath.getAbsolutePath() );
         }
 
-        Result.extraInfo("Dataset features", dataset.getFeaturesSize());
+        Result.extraInfo( "Dataset features", dataset.getFeaturesSize() );
 
-        dataset.setSiftReader(siftReader);
+        dataset.setSiftReader( siftReader );
         return dataset;
 
     }
 
 }
-
