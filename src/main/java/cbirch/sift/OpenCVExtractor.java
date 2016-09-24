527cffcb450970894e48d244edc8ff2f9fc6c315
Luciano Lopes <lucianoshl@gmail.com>
2016-09-24 16:15:59 -0300

    remove opencv

--- src/main/java/cbirch/sift/OpenCVExtractor.java
+++ src/main/java/cbirch/sift/OpenCVExtractor.java
@@ -1,13 +1,6 @@
 package cbirch.sift;
 
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfKeyPoint;
-import org.opencv.features2d.FeatureDetector;
-import org.opencv.features2d.KeyPoint;
-import org.opencv.highgui.Highgui;
-
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
@@ -28,16 +21,17 @@ public class OpenCVExtractor implements SiftExtractor {
 
     @Override
     public int[] extract( File image ) {
-//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat objectImage = Highgui.imread(image.getAbsolutePath(), Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-
-        MatOfKeyPoint objectKeyPoints = new MatOfKeyPoint();
-        FeatureDetector featureDetector = FeatureDetector.create(FeatureDetector.SIFT);
-        featureDetector.detect(objectImage, objectKeyPoints);
-        KeyPoint[] keypoints = objectKeyPoints.toArray();
-        System.out.println(keypoints);
-
-        return new int[ 0 ];
+        throw new UnsupportedOperationException();
+////        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//        Mat objectImage = Highgui.imread(image.getAbsolutePath(), Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+//
+//        MatOfKeyPoint objectKeyPoints = new MatOfKeyPoint();
+//        FeatureDetector featureDetector = FeatureDetector.create(FeatureDetector.SIFT);
+//        featureDetector.detect(objectImage, objectKeyPoints);
+//        KeyPoint[] keypoints = objectKeyPoints.toArray();
+//        System.out.println(keypoints);
+//
+//        return new int[ 0 ];
     }
 
 
