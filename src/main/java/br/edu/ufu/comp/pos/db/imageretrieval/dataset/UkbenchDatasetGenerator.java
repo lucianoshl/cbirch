7b72870be41339265825ede4926e00bcad5e21f0
Luciano Lopes <lucianoshl@gmail.com>
2017-10-02 21:20:51 -0300

    fix

--- src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/dataset/UkbenchDatasetGenerator.java
+++ src/main/java/br/edu/ufu/comp/pos/db/imageretrieval/dataset/UkbenchDatasetGenerator.java
@@ -2,6 +2,7 @@ package br.edu.ufu.comp.pos.db.imageretrieval.dataset;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
@@ -16,8 +17,18 @@ public class UkbenchDatasetGenerator extends DatasetGenerator {
     @SneakyThrows
     public static void main(String[] args) {
 
-        new UkbenchDatasetGenerator(new GeneratedDataset(args[0]),
-                new File(args[1])).generate();
+        List<String> datasets = Arrays.asList("ukbench-00-1000", "ukbench-01-1000", "ukbench-02-1000", "ukbench-03-1000", "ukbench-04-1000", "ukbench-05-1000", "ukbench-06-1000", "ukbench-07-1000", "ukbench-08-1000", "ukbench-09-1000", "ukbench-10200", "ukbench-10-200");
+
+        String workspace = System.getenv().get("DATASET_WORKSPACE");
+        for (String name : datasets) {
+            try {
+                new UkbenchDatasetGenerator(new GeneratedDataset(name),
+                        new File(workspace + "/raw-datasets/" + name)).generate();
+            } catch (Exception e) {
+                System.out.println("ERROORRR IN:" + name);
+                e.printStackTrace();
+            }
+        }
 
     }
 
@@ -26,18 +37,18 @@ public class UkbenchDatasetGenerator extends DatasetGenerator {
         File gtFiles = new File(dataset.base, "gt_files");
         FileUtils.deleteQuietly(gtFiles);
         gtFiles.mkdirs();
-        
+
         File currentFile = null;
-        
+
         File[] listTrainImagesFiles = this.dataset.listTrainImagesFiles();
         for (int i = 0; i < listTrainImagesFiles.length; i++) {
             File file = listTrainImagesFiles[i];
-            if (i%4 == 0){
-                currentFile = new File(gtFiles,file.getName() + ".txt");
+            if (i % 4 == 0) {
+                currentFile = new File(gtFiles, file.getName() + ".txt");
                 currentFile.createNewFile();
-                FileUtils.writeLines(currentFile, Arrays.asList(file.getName()),true);
+                FileUtils.writeLines(currentFile, Arrays.asList(file.getName()), true);
             } else {
-                FileUtils.writeLines(currentFile, Arrays.asList(file.getName()),true);
+                FileUtils.writeLines(currentFile, Arrays.asList(file.getName()), true);
             }
         }
 
